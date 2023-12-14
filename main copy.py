22a6b49ae0bfd2ceb44e9d3c68c09a118d0e8933
unknown <bpomogaibo.gmail.com>
2023-12-14 14:28:16 +0200

    lesson 2

--- main copy.py
+++ main copy.py
@@ -26,3 +26,28 @@ print(f"n1: {n1} n2: {n2} n3: {n3} n4: {n4}")
 print(f"Result: {result}")
 
 ########
+
+# умови
+#v1
+
+# n1 = 10
+# n2 = 20
+
+# v2
+n1, n2 = 10, 20 # множинне привласнення
+
+print(n1 > n2)
+print(n1 >= n2)
+print(n1 < n2)
+print(n1 <= n2)
+print(n1 == n2) # поверне True, якщо обидва операнди рівні (однакові)
+print(n1 != n2, end="\n" "\n") # поверне True, якщо обидва операнди різні
+#
+print(1 == 1 and 3 == 3) # поверне True, якщо обидва операнди рівні True, інакше False 
+print(1 == 1 or 2 == 3) # поверне True, якщо хоча б один операнд дорівнює True, інакше False
+#
+is_valid = False
+print(is_valid)
+print(not is_valid) # not -> інверсіяБ якщо значення False стане True і навпаки
+
+print("hello" in "hello world") # виведе True
