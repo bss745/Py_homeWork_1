0a036d716db36f4bb5a19d6ea65559c07632c2ad
Pomogaibo Borys <129752759+bss745@users.noreply.github.com>
2023-12-14 15:35:01 +0200

    Update main copy.py
    homework completed

--- main copy.py
+++ main copy.py
@@ -95,3 +95,18 @@ elif n3 < n2 < n1:
 	print(n3)
 else:
 	print("All numbers equal!")
+
+
+# ввести з клавіатури 3 числа
+# - вивести кількість однакових чисел
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+n3 = int(input("Enter third number: "))
+
+if n1 == n2 == n3:
+	print(3)
+elif n1 == n2 or n2 == n3 or n1 == n3:
+	print(2)
+else:
+	print(0)
